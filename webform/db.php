5a230357daac9c158e4f864513bfc077783a16e1
Harshitha Ramachandra <115808063+hramac@users.noreply.github.com>
2022-11-27 23:43:45 -0500

    Delete webform directory

--- webform/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "alumnidetails";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
--- webform/dbConnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "loginsystem";
-
-$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
